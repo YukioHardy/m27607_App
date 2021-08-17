25bbc42592be642dffc8e31c467eb6e059605250
YukioHardy <rosalie-thost@hotmail.de>
2021-08-17 10:46:27 +0200

    Splash Screen

---
+++ app/src/main/java/com/example/m27607_app/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.m27607_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(4000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent newIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(newIntent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
